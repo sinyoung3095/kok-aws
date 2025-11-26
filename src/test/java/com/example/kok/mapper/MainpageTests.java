fbe0998808be70e8f43341ea1797eb82df0ddd1a
sinyoung3095 <sinyoung3095@gmail.com>
2025-11-26 15:29:25 +0900

    작업중

--- src/test/java/com/example/kok/mapper/MainpageTests.java
+++ src/test/java/com/example/kok/mapper/MainpageTests.java
@@ -1,46 +1,46 @@
-package com.example.kok.mapper;
-
-import com.example.kok.dto.UserDTO;
-import com.example.kok.dto.UserMemberDTO;
-import com.example.kok.repository.*;
-import com.example.kok.service.MainpageService;
-import com.example.kok.service.MemberService;
-import com.example.kok.service.S3Service;
-import com.example.kok.util.Criteria;
-import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.Duration;
-import java.util.List;
-import java.util.Optional;
-
-@SpringBootTest
-@Slf4j
-public class MainpageTests {
-    @Autowired
-    private S3Service s3Service;
-    @Autowired
-    private MemberMapper memberMapper;
-    @Autowired
-    private MemberDAO memberDAO;
-    @Autowired
-    private RequestExperienceDAO requestExperienceDAO;
-    @Autowired
-    private MemberAlarmSettingDAO  memberAlarmSettingDAO;
-    @Autowired
-    private FollowDAO followDAO;
-    @Autowired
-    private MainpageService mainpageService;
-
-    @Test
-    public void mainpageTest(){
-        if(followDAO.selectAllFollow()>0) {
-            Assertions.assertThat(mainpageService.findPopularCompanies().toString());
-        }
-    }
-
-
-}
+//package com.example.kok.mapper;
+//
+//import com.example.kok.dto.UserDTO;
+//import com.example.kok.dto.UserMemberDTO;
+//import com.example.kok.repository.*;
+//import com.example.kok.service.MainpageService;
+//import com.example.kok.service.MemberService;
+//import com.example.kok.service.S3Service;
+//import com.example.kok.util.Criteria;
+//import lombok.extern.slf4j.Slf4j;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.time.Duration;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@SpringBootTest
+//@Slf4j
+//public class MainpageTests {
+//    @Autowired
+//    private S3Service s3Service;
+//    @Autowired
+//    private MemberMapper memberMapper;
+//    @Autowired
+//    private MemberDAO memberDAO;
+//    @Autowired
+//    private RequestExperienceDAO requestExperienceDAO;
+//    @Autowired
+//    private MemberAlarmSettingDAO  memberAlarmSettingDAO;
+//    @Autowired
+//    private FollowDAO followDAO;
+//    @Autowired
+//    private MainpageService mainpageService;
+//
+//    @Test
+//    public void mainpageTest(){
+//        if(followDAO.selectAllFollow()>0) {
+//            Assertions.assertThat(mainpageService.findPopularCompanies().toString());
+//        }
+//    }
+//
+//
+//}
